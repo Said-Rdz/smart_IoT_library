2ab0a8593d126ae18431f34b7d2e7e525c760874
Said-Rdz <75096154+Said-Rdz@users.noreply.github.com>
2022-07-26 17:59:51 -0700

    Update smartiot.py
    Añadiendo función print_info para imprimir información del archivo.

--- smartiot.py
+++ smartiot.py
@@ -6,7 +6,7 @@ def version():
   '''
     Shows smart IoT library version
   '''
-  print('Smart IoT Library ver. 1.1.0')
+  print('Smart IoT Library ver. 1.2.0')
   
 def load_audio(url, fname):
   '''
@@ -21,3 +21,22 @@ def load_audio(url, fname):
   wave, sr = torchaudio.load(fname)
 
   return (wave, sr, meta, sz)
+
+def print_info(info, fname=None):
+  if fname:
+    print('-'*30)
+    print('Filename: '+fname)
+    print('-'*30)
+
+  wave, sr, meta, sz= info
+  channels, frames = wave.shape
+  print(f'            Frames: {frames}')
+  print(f'          Channels: {channels}')
+  print(f'   Audio File Size: {sz} bytes')
+  print(f'Tensor memory size: {wave.element_size() * channels * frames} bytes')
+  print(f'             Dtype: {wave.dtype}')
+  print(f'               Max: {wave.max().item():6.3f}')
+  print(f'               Min: {wave.min().item():6.3f}')
+  print(f'              Mean: {wave.mean().item():6.3f}')
+  print(f'           Std Dev: {wave.max().std():6.3f}')
+  print(wave)
